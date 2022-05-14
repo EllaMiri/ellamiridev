7393b9224245ffd6d0798089918c6a354854c2b9
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-14 17:53:33 +0200

    started on home / front page

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-text-container">
+        <span>Hi there! My name is</span>
+        <h2>Ella Larsson</h2>
+        <span>and I'm a front end developer</span>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
