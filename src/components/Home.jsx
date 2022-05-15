b4beca63f87cc1c20be08d31388b9a91cf0ca5bc
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-15 16:50:38 +0200

    added memoji

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
 import "./Home.css";
+import memoji from "../assets/ellamiri_memoji.mov";
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-text-container">
-        <span>Hi there! My name is</span>
-        <h2>Ella Larsson</h2>
-        <span>and I'm a front end developer</span>
+        <p className="hi-there">Hi there! My name is</p>
+        <h2 className="name">Ella Larsson</h2>
+        <p className="occupation">and I'm a front end developer</p>
+        <video
+          className="memoji"
+          autoPlay
+          loop
+          muted
+          playsInline
+          // controls
+          // src="./assets/memoji.mp4"
+          // type="video/mp4"
+          src={memoji}
+        >
+          {/* <source src={memoji} /> */}
+        </video>
       </div>
     </div>
   );
