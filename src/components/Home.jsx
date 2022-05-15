c1545fe7082b5414d796b6f32bf2454e152b66a5
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-15 17:50:56 +0200

    added fontawesome icons for contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import "./Home.css";
 import memoji from "../assets/ellamiri_memoji.mov";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faSnapchatSquare } from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
   return (
@@ -8,6 +13,24 @@ function Home() {
         <p className="hi-there">Hi there! My name is</p>
         <h2 className="name">Ella Larsson</h2>
         <p className="occupation">and I'm a front end developer</p>
+        <div className="contact-icons">
+          <span>
+            {" "}
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+          <span>
+            {" "}
+            <FontAwesomeIcon icon={faLinkedin} />
+          </span>
+          <span>
+            {" "}
+            <FontAwesomeIcon icon={faGithub} />
+          </span>
+          <span>
+            {" "}
+            <FontAwesomeIcon icon={faSnapchatSquare} />
+          </span>
+        </div>
         <video
           className="memoji"
           autoPlay
