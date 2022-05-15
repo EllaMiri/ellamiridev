c1bd328c840194921af4a980e518a9a2a055fc02
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-15 17:18:41 +0200

    added router link for all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <div className="header-left">
         {/* TODO: Add burgermenu here */}
-        <span>ellamiri</span>
+        <Link style={{ textDecoration: "none" }} to="/">
+          <span>ellamiri</span>
+        </Link>
       </div>
       <nav className="header-right">
         <ul className="nav-links">
-          <li className="list-item">My work</li>
-          <li className="list-item">Skills</li>
-          <li className="list-item">About me</li>
-          <li className="list-item">Contact</li>
+          <Link style={{ textDecoration: "none" }} to="MyWork">
+            <li className="list-item">My work</li>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="Skills">
+            <li className="list-item">Skills</li>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="AboutMe">
+            <li className="list-item">About Me</li>
+          </Link>
+          <Link style={{ textDecoration: "none" }} to="Contact">
+            <li className="list-item">Contact</li>
+          </Link>
         </ul>
       </nav>
     </header>
