f82c8afcc9b1aabf0cf49729f8a5b61744828f73
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-15 17:07:05 +0200

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Header />} /> */}
+      </Routes>
     </div>
   );
 }
