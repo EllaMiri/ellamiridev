37947f2d6994653116350756892c7a13e276d506
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-16 15:48:49 +0200

    added favicon and document title

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import MyWork from "./components/MyWork";
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "ellamiri";
+  }, []);
   return (
     <div>
       <Header />
