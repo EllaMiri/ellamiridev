52a10b46b155ece8007471e80bd3169bed72f593
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-16 14:01:24 +0200

    added my work project text

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import MyWork from "./components/MyWork";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/MyWork" element={<MyWork />} />
         {/* <Route path="/" element={<Header />} /> */}
       </Routes>
     </div>
---
+++ src/components/MyWork.jsx
@@ -0,0 +1,26 @@
+import "./MyWork.css";
+
+function MyWork() {
+  return (
+    <div>
+      <div className="projects-text-container">
+        <h1 className="projects-header">Projects</h1>
+        <p className="projects-info-text">
+          During my front end education I've created a wide variety of projects
+          ranging from games to webshops <br /> and full stack applications with
+          user registration and login using MongoDB. <br /> Here I've put them
+          together along with projects I've created on my freetime for you to
+          view and try the demos.
+        </p>
+        <p>
+          My goal as a developer is to create websites / applications that are
+          easy to use, visually appealing and has a purpose. <br /> I focus a
+          lot on the user experience since the users are the reason the
+          application exists.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MyWork;
